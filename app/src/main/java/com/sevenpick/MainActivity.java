37e0737192d50973fdb30b2c05327e608b4a2ee4
tanzhenglian <tanzhenglian@beescm.cn>
2017-08-14 11:58:49 +0800

    集成bugly全量更新

--- app/src/main/java/com/sevenpick/MainActivity.java
+++ app/src/main/java/com/sevenpick/MainActivity.java
@@ -4,7 +4,6 @@ import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.roughike.bottombar.BottomBar;
--- app/src/main/java/com/sevenpick/fragment/UserFragment.java
+++ app/src/main/java/com/sevenpick/fragment/UserFragment.java
@@ -2,12 +2,20 @@ package com.sevenpick.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
 
 import com.sevenpick.R;
 import com.sevenpick.framework.BaseFragment;
 
+import butterknife.BindView;
+import io.reactivex.annotations.Beta;
+
 
 public class UserFragment extends BaseFragment {
+
+    @BindView(R.id.check_version)
+    Button btn;
 
     @Override
     protected int getLayoutResource() {
@@ -17,5 +25,12 @@ public class UserFragment extends BaseFragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 }
